11a0602ebc7e593bf0b94537796f511eab6605c1
venkat1702 <myv1702@gmail.com>
2022-02-12 10:50:53 +0530

    intial commit

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,40 @@
+const Sidebar = ({
+  notes,
+  onAddNote,
+  onDeleteNote,
+  activeNote,
+  setActiveNote,
+}) => {
+  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1 style={{color:"white"}}>ReKord 📚</h1>
+        <button onClick={onAddNote}><i class="ri-add-line"></i></button>
+      </div>
+      <div className="app-sidebar-notes">
+        {sortedNotes.map(({ id, title, body, lastModified }, i) => (
+          <div
+            className={`app-sidebar-note ${id === activeNote && "active"}`}
+            onClick={() => setActiveNote(id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{title && title.substr(0,20)}</strong>
+              <button onClick={(e) => onDeleteNote(id)}><i class="ri-delete-bin-2-line"></i></button>
+            </div>
+            <small className="note-meta">
+              Last Modified{" "}
+              {new Date(lastModified).toLocaleDateString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
